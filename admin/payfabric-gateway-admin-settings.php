c8fa518ce1dc63f712bfe54911157e826849630d
Jack Zhang <Jack.Zhang@nodus.com>
2022-07-12 11:23:04 +0800

    update description format

--- admin/payfabric-gateway-admin-settings.php
+++ admin/payfabric-gateway-admin-settings.php
@@ -96,7 +96,7 @@ if ($integration_show) {
         'title' => __('Payment mode', 'payfabric-gateway-woocommerce'),
         'type' => 'select',
         'description' => sprintf(__('Payment Mode controls the presentation of the Hosted Payment Page (HPP):<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;<b>• Direct:</b> HPP is inside the shopping site page.<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;<b>• Iframe:</b> HPP is inside the shopping site page.<br>
             &nbsp;&nbsp;&nbsp;&nbsp;<b>• Redirect:</b> Shopping site redirects user to the HPP.<br>
             &nbsp;&nbsp;&nbsp;&nbsp;<b>• Direct:</b> HPP shown directly on the checkout page, payment made when placing order. (A theme is required, see %sGuide%s) '), '<a href="https://github.com/PayFabric/WooCommerce-Plugin#readme" target="_blank">', '</a>' ),
         'desc_tip' => false,
